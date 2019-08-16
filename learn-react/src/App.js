a0d6d824ca712466aa0fe82f2db09f9254bceb75
rai8 <hytonne@outlook.com>
2019-08-16 18:43:08 +0300

    storing and rendering data using axios

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,9 +1,31 @@
-import "./App.css";
-
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+  getUsers() {
+    axios("https://api.randomuser.me").then(response =>
+      this.setState({ users: response.data.results })
+    );
+  }
+  componentWillMount() {
+    this.getUsers();
+  }
   render() {
-    return <div>Hello world</div>;
+    return (
+      <div className="App">
+        {this.state.users.map(user => (
+          <div>
+            <p>{user.cell}</p>
+            <p>{user.email}</p>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
